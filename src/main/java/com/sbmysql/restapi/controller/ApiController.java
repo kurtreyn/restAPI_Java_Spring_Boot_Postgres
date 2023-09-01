84416dc88b9a6c4b4a8a46d526445d38719a5b90
kurtreyn <kurt.reynolds@twinspires.com>
2023-09-01 14:07:34 -0400

    refactored code to match best practices

--- src/main/java/com/sbmysql/restapi/controller/ApiController.java
+++ src/main/java/com/sbmysql/restapi/controller/ApiController.java
@@ -1,74 +1,113 @@
 package com.sbmysql.restapi.controller;
 
-import com.sbmysql.restapi.models.User;
+import com.sbmysql.restapi.entities.UserEntity;
+import com.sbmysql.restapi.models.UserModel;
 import com.sbmysql.restapi.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ApiController {
-    @Autowired
+//    @Autowired
     UserRepo userRepo;
+
+    public ApiController(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
 
 
     @GetMapping("/")
     public String getPage() {
-        return "Hello World!";
+        return "Server is running";
     }
 
     @GetMapping("/users")
-    public List<User> getUsers() {
-        return userRepo.findAll();
+    public ResponseEntity<List<UserModel>> getUsers() {
+        List<UserEntity> users = userRepo.findAll();
+        List<UserModel> userModels = new ArrayList<>();
+        for (UserEntity user : users) {
+            UserModel userModel = new UserModel();
+            userModel.setId(user.getId());
+            userModel.setName(user.getName());
+            userModel.setEmail(user.getEmail());
+            userModels.add(userModel);
+        }
+        return new ResponseEntity<List<UserModel>>(userModels, HttpStatus.OK);
     }
+
 
     @GetMapping("/users/{id}")
-    public User getUser(@PathVariable long id) {
-        return userRepo.findById(id).get();
+    public ResponseEntity<UserModel> getUser(@PathVariable long id) {
+        UserEntity user = userRepo.findById(id).get();
+        UserModel userModel = new UserModel();
+        userModel.setId(user.getId());
+        userModel.setName(user.getName());
+        userModel.setEmail(user.getEmail());
+        return new ResponseEntity<>(userModel, HttpStatus.OK);
     }
+
 
     @GetMapping("/users/email")
-    public User getUserByEmail(@RequestParam String email) {
+    public ResponseEntity<UserModel> getUserByEmail(@RequestParam String email) {
 //        http://localhost:8080/users/email?email=kurt@email.com
-        return userRepo.findByEmail(email);
+        Optional<UserEntity> userOptional = Optional.ofNullable(userRepo.findByEmail(email));
+        if (!userOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            UserEntity user = userOptional.get();
+            UserModel userModel = new UserModel();
+            userModel.setId(user.getId());
+            userModel.setName(user.getName());
+            userModel.setEmail(user.getEmail());
+            return new ResponseEntity<>(userModel, HttpStatus.OK);
+        }
     }
+
 
     @GetMapping("/users/name")
-    public User getUserByName(@RequestParam String name) {
+    public ResponseEntity<UserModel> getUserByName(@RequestParam String name) {
 //        http://localhost:8080/users/name?name=Kurt
-        return userRepo.findByFirstName(name);
+        Optional<UserEntity> userOptional = Optional.ofNullable(userRepo.findByName(name));
+        if (userOptional.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            UserEntity user = userOptional.get();
+            UserModel userModel = new UserModel();
+            userModel.setId(user.getId());
+            userModel.setName(user.getName());
+            userModel.setEmail(user.getEmail());
+            return new ResponseEntity<>(userModel, HttpStatus.OK);
+        }
     }
-
-//    @GetMapping("/users/name")
-//    public ResponseEntity<List<User>> getUserByName(@RequestParam String name) {
-//        List username = userRepo.findByFirstName(name);
-//        System.out.println("username: " + username);
-//        return new ResponseEntity<List<User>>(userRepo.findByFirstName(name), HttpStatus.OK);
-//    }
-
 
     @PostMapping("/users")
-    public String saveUser(@RequestBody User user) {
-        userRepo.save(user);
-        return "User saved";
+    public ResponseEntity saveUser(@RequestBody UserModel user) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setName(user.getName());
+        userEntity.setEmail(user.getEmail());
+        userRepo.save(userEntity);
+        return new ResponseEntity(user, HttpStatus.CREATED);
     }
 
+
     @PutMapping("/users/{id}")
-    public String updateUser(@PathVariable long id, @RequestBody User user){
-        User updatedUser = userRepo.findById(id).get();
-        updatedUser.setFirstName(user.getFirstName());
-        updatedUser.setLastName(user.getLastName());
+    public ResponseEntity updateUser(@PathVariable long id, @RequestBody UserModel user){
+        UserEntity updatedUser = userRepo.findById(id).get();
+        updatedUser.setName(user.getName());
         updatedUser.setEmail(user.getEmail());
         userRepo.save(updatedUser);
-        return "User updated";
+        return new ResponseEntity(user, HttpStatus.OK);
     }
 
     @DeleteMapping("/users/{id}")
-    public String deleteUser(@PathVariable long id){
+    public ResponseEntity deleteUser(@PathVariable long id) {
         userRepo.deleteById(id);
-        return "User deleted";
+        return new ResponseEntity("user deleted",HttpStatus.OK);
     }
 }
